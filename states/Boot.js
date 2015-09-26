150889367c42799f3c915f312ca2041d2033c357
Fernando Larrañaga <xabadu@gmail.com>
2015-09-26 20:00:32 -0300

    Add basic config, clean comments and some images

--- states/Boot.js
+++ states/Boot.js
@@ -18,11 +18,9 @@ AmebaAdventures.Boot.prototype = {
         this.scale.pageAlignVertically = true;
 
         if (this.game.device.desktop) {
-            console.log("I'm desktop");
             this.scale.maxWidth = this.game.width;
             this.scale.maxHeight = this.game.height;
         } else {
-            console.log("I'm mobile");
             this.scale.maxWidth = this.game.width * 2.5;
             this.scale.maxHeight = this.game.height * 2.5;
             this.scale.forceOrientation(false, true);
